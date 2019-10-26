e1008d44a768cc0c633f04b57bf1a24607d184b2
Paulo Marques <paulodsam@gmail.com>
2019-10-26 16:44:48 +0100

    first model

---
+++ bird_chat/lib/data/participant_model.dart
@@ -0,0 +1,13 @@
+class Participant{
+
+  final String name;
+  final String affiliation;
+  final String key;
+  final String url;
+  final String bio;
+  final String urlPhoto;
+
+  Participant({this.name,this.affiliation,this.key,this.url,this.urlPhoto,this.bio});
+  //todo had a http request to the database for the persons details, for that json will be needed
+
+}
