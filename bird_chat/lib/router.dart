2c79c834adfaa4c8b3ae338787fc31b68252103c
Paulo Marques <paulodsam@gmail.com>
2019-11-07 17:00:14 +0000

    deleted old home view

--- bird_chat/lib/router.dart
+++ bird_chat/lib/router.dart
@@ -1,12 +1,11 @@
 import 'package:bird_chat/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:bird_chat/screens/CreatePage.dart';
-import 'package:bird_chat/screens/HomeView.dart';
 import 'package:bird_chat/screens/UndefinedView.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings){
   switch(settings.name){
-    case HomeView.route:
+    case Home.route:
       return MaterialPageRoute(builder: (context) => Home());
     case CreatePage.route:
       return MaterialPageRoute(builder: (context) => CreatePage());
