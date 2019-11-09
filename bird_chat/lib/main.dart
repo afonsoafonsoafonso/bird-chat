33439c38a334329c398a27c10fb545e6e096effa
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-11-09 13:18:11 +0000

    update: isto afinal ficou um bocado merda. cards all the way

--- bird_chat/lib/main.dart
+++ bird_chat/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:bird_chat/screens/home.dart';
-import 'package:bird_chat/widgets/home_body2.dart';
+// isto afinal ficou um bocado merda
 
 void main() {
-
-  List<EventOverview> events = generateEvents(30);
 
   runApp(
     MaterialApp(
