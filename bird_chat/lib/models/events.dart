2ad3730a07d69d6ae0e825712d781a01db4e4f5f
Moisés Rocha <moisesrocha99@gmail.com>
2019-11-25 17:48:12 +0000

    updated Event class to have an id and creator key

--- bird_chat/lib/models/events.dart
+++ bird_chat/lib/models/events.dart
@@ -2,16 +2,20 @@ import 'package:bird_chat/models/startTime.dart';
 
 class Event {
 
+  int id;
+  String creator;
   String title;
   String location;
   StartTime startTime;
   String description;
   List<String> tags;
   
-  Event({this.title, this.location, this.startTime, this.description, this.tags});
+  Event({this.id, this.creator, this.title, this.location, this.startTime, this.description, this.tags});
 
   factory Event.fromJson(Map<String,dynamic> json) {
     return Event (
+      id: json['id'] as int,
+      creator: json['creator'] as String,
       title: json['title'] as String,
       location: json['location'] as String,
       startTime: StartTime.fromJson(
